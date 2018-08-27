f09052ef6cfe09d12ec26e9268ce8c6db034702c
paarmita <paarmita1998@gmail.com>
2018-08-27 23:22:14 +0530

    sign in UI add

---
+++ src/components/SignIn.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './SignIn.css';
+
+class Signin extends Component {
+  render() {
+    return (
+      <div className="row valign-wrapper social-signin-container">
+        <div className="col s4 offset-s4">
+          <a href="#" className="social-signin">
+            <i className="fa fa-google social-signin-icon" />
+            Sign In With Google
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+ export default connect(null, null)(Signin);
